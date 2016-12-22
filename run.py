f17915b07823fc8c9a465f0a716dbeabb7cc9221
Ben Brady <bradybt@umich.edu>
2016-12-21 18:24:58 -0600

    subbing almost done

--- run.py
+++ run.py
@@ -21,12 +21,19 @@ def subPlayers():
 	playerName = request.form.get("bench",None)
 	if playerName == None:
 		users = pickle.load(open("playerInfo.p", "rb"))
+		statMap = pickle.load(open("statMap.p", "rb"))
 		playerName = request.form.get("subbed-player-name",None)
 		playerAttr = request.form.get("subbed-player-attr",None)
-		
+
+		for user in users:
+			for player in user.team:
+
+				if player.name == playerName and player.attribute = statMap[playerAttr]:
+					return render_template('ben.html', users=users) 
+
 		for user in users:
 			if user.name == "Ben":
-				user.addPickUp(playerName,'m',playerAttr)
+				user.addPickUp(playerName,'m',statMap[playerAttr])
 				return render_template('ben.html', users=users)
 	else:
 		user = sub.moveFromBench(playerName,"Ben")
