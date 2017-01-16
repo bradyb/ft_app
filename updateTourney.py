0d19e2b0b073aecdc82ce4dfd02d571a21ed6152
Ben Brady <bradybt@umich.edu>
2017-01-16 14:32:57 -0600

    and we back

--- updateTourney.py
+++ updateTourney.py
@@ -3,94 +3,69 @@ from lxml import html
 import requests
 import operator
 import datetime
-
+from bs4 import BeautifulSoup
+import urllib2
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 ## maybe this should be broken up later ##
 
-def getPoints(playerName, index, baseURL, playerAttr, statMap, win, links, neededStats):
 
-	winnersStats = []
-	losersStats = []
-	#linkIndex = sideList.index(playerName)
-	matchStatsURL = baseURL + links[index]
+def getPoints(name, attribute,	order, statNames, pageSource):
+	soup = BeautifulSoup(pageSource, "lxml")
 
-	statsPage = requests.get(matchStatsURL)
-	tree1 = html.fromstring(statsPage.content)
+	if attribute == 1:
+		print soup.find('div', {'id': statNames[0] + order}).text
+		print statNames[0] + order
+		aces = int(soup.find('div', {'id': statNames[0] + order}).text)
+		dfs = int(soup.find('div', {'id': statNames[1] + order}).text)
+		return 3 *(aces - dfs)
 
-	stats = tree1.xpath('//*[@id="matchStatsData"]/text()')
-	name = tree1.xpath('//*[@id="modalScoresMatchStatsTable"]/div[1]/div[1]/div[2]/a/span[2]/text()')
+	elif attribute == 2:
+		winners = int(soup.find('div', {'id': statNames[2] + order}).text)
+		ues = int(soup.find('div', {'id': statNames[3] + order}).text)
+		return 2 * winners - ues
 
-	if playerName.split(' ')[1] in name[0]:
-		win = 1;
-	else: 
-		win = 0
+	elif attribute == 3:
+		print soup.find('div', {'id': statNames[5] + order}).text
+		return int(soup.find('div', {'id': statNames[5] + order}).text.split('(')[1].split('%')[0])
 
-	statsList = stats[0].split('\r\n')
+	elif attribute == 4:
+		print soup.find('div', {'id': statNames[4] + order}).text
+		print statNames[4] + order
+		return int(soup.find('div', {'id': statNames[4] + order}).text.split('(')[1].split('%')[0])
+		
 
-	stats = -1
+	elif attribute == 5:
+		if order == '1':
+			bpc = int(soup.find('div', {'id': statNames[6] + order}).text.split('/')[0])
+			bps = int(soup.find('div', {'id': statNames[6] + '2'}).text.split('/')[1].split(' ')[0])
 
-	counter = 0
+			bpl = int(soup.find('div', {'id': statNames[6] + '2'}).text.split('/')[0])
 
-
-	for item in statsList:
-
-		if "\"setNum\": 1," in item:
-			break
-		#print item
-		#print neededStats[counter]
-		if neededStats[counter] in item:
-
-			itemList = item.split(' ')
-			stats = int(itemList[len(itemList) - 1][:-1])
-
-			if len(winnersStats) < len(neededStats):
-				winnersStats.append(stats)
-			else:
-				losersStats.append(stats)
-			
-			counter = (counter + 1) % len(neededStats)
-
-
-	if playerAttr == 1:
-		if win:
-			return 3 * (winnersStats[0] - winnersStats[1])
+			return 5*(bps - bpl + bpc) - 3 * bpl
 		else:
-			return 3 * (losersStats[0] - losersStats[1])
+			bpc = int(soup.find('div', {'id': statNames[6] + order}).text.split('/')[0])
+			bps = int(soup.find('div', {'id': statNames[6] + '1'}).text.split('/')[1].split(' ')[0])
 
-	elif playerAttr == 2:
-		numWinners = int(raw_input("Number Winners for " + playerName + "?"))
-		numUE = int(raw_input("Number of Unforced Errors?"))
-		return 2 * numWinners - numUE
+			bpl = int(soup.find('div', {'id': statNames[6] + '1'}).text.split('/')[0])
 
-	elif playerAttr == 3:
-		if win:
-			return winnersStats[5]
-		else:
-			return losersStats[5]
-
-	elif playerAttr == 4:
-		if win:
-			return winnersStats[2]
-		else:
-			return losersStats[2]
-
-
-	elif playerAttr == 5:
-		if win:
-			return 5 * (losersStats[4] - losersStats[3] + winnersStats[3]) - 3 * (winnersStats[4] - winnersStats[3])
-		else:
-			return 5 * (winnersStats[4] - winnersStats[3] + losersStats[3]) - 3 * (losersStats[4] - losersStats[3])
+			return 5*(bps - bpl + bpc) - 3 * bpl
 
 
 
-def updateLeague(todayDate = None):
+def updateLeague(tourneyDay,todayDate = None):
 
 	## TODO: tourney map of name to code ##
 	## does the name change from year to year? It must, right? ##
 
-	tourneyName = "brisbane"
+	tourneyName = "australian-open"
 
-	tourneyCode = "339"
+	tourneyCode = "580"
 
 	tourneyData = pickle.load(open( "playerInfo.p", "rb" ))
 
@@ -100,32 +75,31 @@ def updateLeague(todayDate = None):
 		today = datetime.datetime.now()
 		todayDate =  str(today.month) + '/' + str(today.day) + '/' + str(today.year)
 
-	print todayDate
+	#baseURL = "http://www.atpworldtour.com"
 
-	baseURL = "http://www.atpworldtour.com"
+	baseURL = "http://www.ausopen.com/en_AU/scores/completed_matches/day"
 
-	urlStr = "http://www.atpworldtour.com/en/scores/current/" + tourneyName + '/' + tourneyCode + '/' + 'results?matchdate=' + todayDate
+	#urlStr = "http://www.atpworldtour.com/en/scores/current/" + tourneyName + '/' + tourneyCode + '/' + 'results?matchdate=' + todayDate
+
+	urlStr = baseURL + tourneyDay + ".html"
 
 	page = requests.get(urlStr)
 	tree = html.fromstring(page.content)
 
-	winners = tree.xpath('//*[@id="scoresResultsContent"]/div/table/tbody/tr/td[3]/a/text()')
-
-	print winners
-
-	losers = tree.xpath('//*[@id="scoresResultsContent"]/div/table/tbody/tr/td[7]/a/text()')
-
-	print losers
+	players = tree.xpath('//*[@id="crtcontent"]/div/div[1]/a/text()')
 
 
-	links = tree.xpath('//*[@id="scoresResultsContent"]/div/table/tbody/tr/td[8]/a/@href')
+	links = tree.xpath('//*[@id="results"]/div/div[4]/div/a/@href')
 
 	pointsEarned = 0
 
-	neededStats = ["Aces", "DoubleFaults", "SecondServePointsWonPercentage", 
-						"BreakPointsSavedDividend", "BreakPointsSavedDivisor", "TotalReturnPointsWonPercentage"]
+	neededStats = ["aces_p", "double_faults_p", "winners_p", 
+		"unforced_errors_p", "second_srv_pts_won_p", "rec_pts_won_p",
+			"brk_pts_won_p"]
 
-	orderBool = 0
+	wd = webdriver.Firefox()
+
+	orderBool = '1'
 	for user in tourneyData:
 
 		for player in user.team:
@@ -136,33 +110,43 @@ def updateLeague(todayDate = None):
 				player.history.append((todayDate, 'OUT'))
 				continue
 
-			if player.name in winners:
-				# does this ordering invariant always hold?
-				#orderBool = (player.name.split(' ')[1] < losers[winners.index(player.name)].split(' ')[1])
+			shortName = player.name[0] + ". " + player.name.split(' ')[1]
 
-				pointsEarned = getPoints(player.name, winners.index(player.name), baseURL, player.attribute, 
-								statMap, orderBool, links, neededStats)
+			if shortName in players:
 
-			elif player.name in losers:
+				playerIndex = players.index(shortName)
 
-				#orderBool = (player.name.split(' ')[1] < winners[losers.index(player.name)].split(' ')[1])
+				if playerIndex % 2 == 0:
+					orderBool = '1'
+				else:
+					orderBool = '2'
+
+				wd.get("https://www.ausopen.com" + links[playerIndex / 2])
+
+				time.sleep(5)
+
+				pointsEarned = getPoints(player.name, player.attribute,	orderBool, neededStats, wd.page_source)
+
+			# elif player.name in losers:
+
+			# 	#orderBool = (player.name.split(' ')[1] < winners[losers.index(player.name)].split(' ')[1])
 				
-				pointsEarned = getPoints(player.name, losers.index(player.name), baseURL, player.attribute, 
-								statMap, orderBool, links, neededStats)
+			# 	pointsEarned = getPoints(player.name, losers.index(player.name), baseURL, player.attribute, 
+			# 					statMap, orderBool, links, neededStats)
 
-				player.alive = 0
+			# 	player.alive = 0
 
 			else:
-                dailyStatus = "DNP"
-                if player.sex == 'f':
-                    femaleResult = raw_input("Score for " + player.name + " " + player.attr  +  ": ")
-                    if femaleResult.isdigit() or femaleResult[1:].isdigit:
-                        pointsEarned = int(femaleResult)
-                    elif femaleResult == "OUT":
-                        player.alive = 0
-                        dailyStatus = femaleResult
+				dailyStatus = "DNP"
+				# if player.sex == 'f':
+				# 	femaleResult = raw_input("Score for " + player.name + " " + player.attr  +  ": ")
+				# 	if femaleResult.isdigit() or femaleResult[1:].isdigit:
+				# 		pointsEarned = int(femaleResult)
+				# 	elif femaleResult == "OUT":
+				# 		player.alive = 0
+				# 		dailyStatus = femaleResult
 
-                player.history.append((todayDate, 'DNP'))
+				player.history.append((todayDate, dailyStatus))
 				continue
 
 			player.points = player.points + pointsEarned
@@ -177,33 +161,34 @@ def updateLeague(todayDate = None):
 				benchPlayer.history.append((todayDate, 'OUT'))
 				continue
 
-			if benchPlayer.name in winners:
+			shortName = benchPlayer.name[0] + ". " + benchPlayer.name.split(' ')[1]
 
-				orderBool = (benchPlayer.name.split(' ')[1] < losers[winners.index(benchPlayer.name)].split(' ')[1])
+			if shortName in players:
 
-				pointsEarned = getPoints(benchPlayer.name, winners.index(benchPlayer.name), baseURL, benchPlayer.attribute, 
-								statMap, orderBool, links, neededStats)
+				playerIndex = players.index(shortName)
 
-			elif benchPlayer.name in losers:
-				
-				orderBool = (benchPlayer.name.split(' ')[1] < winners[losers.index(benchPlayer.name)].split(' ')[1])
+				if playerIndex % 2 == 0:
+					orderBool = '1'
+				else:
+					orderBool = '2'
 
-				pointsEarned = getPoints(benchPlayer.name, losers.index(benchPlayer.name), baseURL, benchPlayer.attribute, 
-								statMap, orderBool, links, neededStats)
+				wd.get("https://www.ausopen.com" + links[playerIndex / 2])
 
-				benchPlayer.alive = 0
+				time.sleep(5)
+
+				pointsEarned = getPoints(benchPlayer.name, benchPlayer.attribute,	orderBool, neededStats, wd.page_source)
 
 			else:
-                dailyStatus = "DNP"
-                if player.sex == 'f':
-                    femaleResult = raw_input("Score for " + player.name + " " + player.attr  +  ": ")
-                    if femaleResult.isdigit() or femaleResult[1:].isdigit:
-                        pointsEarned = int(femaleResult)
-                    elif femaleResult == "OUT":
-                        player.alive = 0
-                        dailyStatus = femaleResult
+				dailyStatus = "DNP"
+				# if player.sex == 'f':
+				# 	femaleResult = raw_input("Score for " + player.name + " " + player.attr  +  ": ")
+				# 	if femaleResult.isdigit() or femaleResult[1:].isdigit:
+				# 		pointsEarned = int(femaleResult)
+				# 	elif femaleResult == "OUT":
+				# 		player.alive = 0
+				# 		dailyStatus = femaleResult
 
-                player.history.append((todayDate, 'DNP'))
+				benchPlayer.history.append((todayDate, dailyStatus))
 				continue
 
 			benchPlayer.history.append((todayDate,'(' + str(pointsEarned) + ')'))
@@ -211,8 +196,9 @@ def updateLeague(todayDate = None):
 	sorted(tourneyData, key=lambda player: player.total)
 
 	pickle.dump( tourneyData, open( "playerInfo.p", "wb" ) )
+	wd.quit()
 
 if __name__ == "__main__":
-	date = raw_input('Enter today''s date: ')
+	date = raw_input('Enter the tournament day: ')
 	updateLeague(date)
 
