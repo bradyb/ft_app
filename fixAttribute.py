051270153ebb6f09b2b6dc95cf6454b671b5cb89
Ben Brady <bradybt@umich.edu>
2017-01-21 17:30:46 -0600

    subs

--- fixAttribute.py
+++ fixAttribute.py
@@ -3,10 +3,7 @@ import cPickle as pickle
 tourneyData = pickle.load(open( "playerInfo.p", "rb" ))
 
 for user in tourneyData:
-	if user.name == 'Jack':
-		for player in user.bench:
-			if player.name == 'Dominic Thiem':
-				player.attribute = 1
-				print 'here'
+	if user.name == 'Geoff':
+		user.addPickUp('Dominic Thiem', 'm', 4)
 
 pickle.dump( tourneyData, open( "playerInfo.p", "wb" ) )
