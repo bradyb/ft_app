cb5625bba4538e48e6796d6e11d70f0bd969bd8f
Ben Brady <bradybt@umich.edu>
2017-04-17 22:06:47 -0500

    starting daily insertion connections

---
+++ insertDailyResults.py
@@ -0,0 +1,24 @@
+import cPickle as pickle
+from playerTypes import fPlayer, tPlayer
+import datetime
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from tabledef import *
+
+def insertDailyResults():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+	insertDailyResults()
