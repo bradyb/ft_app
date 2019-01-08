ebfe5282d0c3ecfee9039e0d24c1c508d8abab3c
Ben Brady <bradybt@umich.edu>
2019-01-07 22:56:29 -0800

    Revert "getting logging in up and running"
    This reverts commit 33423117f86a106541d382d70081e4f0d6b3a248.

--- run.py
+++ run.py
@@ -1,10 +1,7 @@
-from flask import Flask, render_template, request, session, flash
+from flask import Flask, render_template, request, session
 import cPickle as pickle
 import substitute as sub
 import os
-from sqlalchemy.orm import sessionmaker
-from tabledef import *
-engine = create_engine('sqlite:///users.db', echo=True)
 app = Flask(__name__)
 
 
@@ -19,7 +16,7 @@ def home():
 @app.route('/<username>')
 def teamPage(username):
     if not session.get('logged_in'):
-        return home()
+        return render_template('login.html')
 	users = pickle.load(open("playerInfo.p", "rb"))
 	for user in users:
 		if username == user.name:
@@ -30,7 +27,7 @@ def teamPage(username):
 @app.route('/<username>', methods=['GET', 'POST'])
 def subPlayers(username):
     if not session.get('logged_in'):
-        return home()
+        return render_template('login.html')
 	playerName = request.form.get("bench",None)
 	if playerName == None:
 		users = pickle.load(open("playerInfo.p", "rb"))
@@ -55,42 +52,6 @@ def subPlayers(username):
 		return render_template('ben.html', user=user, teamName=username)
 	return 'error'
 
-@app.route("/login", methods=['POST'])
-def login():
-	POST_USERNAME = str(request.form['username'])
-	POST_PASSWORD = str(request.form['password'])
- 
-	Session = sessionmaker(bind=engine)
-	s = Session()
-	query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
-	result = query.first()
-	if result:
-		session['logged_in'] = True
-	else:
-		flash('wrong password!')
-	return home()
-
-
-@app.route("/logout")
-def logout():
-    session['logged_in'] = False
-    return home()
-
-@app.route('/test')
-def test():
- 
-    POST_USERNAME = "python"
-    POST_PASSWORD = "python"
- 
-    Session = sessionmaker(bind=engine)
-    s = Session()
-    query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
-    result = query.first()
-    if result:
-        return "Object found"
-    else:
-        return "Object not found " + POST_USERNAME + " " + POST_PASSWORD
 
 if __name__ == "__main__":
-	app.secret_key = os.urandom(12)
 	app.run()
