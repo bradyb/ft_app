e1a7a7a8f139d8f21d9011709954b474818d63f6
Ben Brady <bradybt@umich.edu>
2019-01-07 22:56:25 -0800

    Revert "see if db pushing works. it should?"
    This reverts commit d74c01ec7af5f9dc5ddbbabe22eb01ee98e4ffcd.

--- run.py
+++ run.py
@@ -69,6 +69,7 @@ def subPlayers(username):
 	return 'error'
 
 
+
 @app.route("/login", methods=['POST'])
 def login():
 	POST_USERNAME = str(request.form['username'])
@@ -76,14 +77,15 @@ def login():
  
 	Session = sessionmaker(bind=engine)
 	s = Session()
-	query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
-	result = query.first()
-	if result:
-		session['logged_in'] = True
-		session['username'] = POST_USERNAME
-		print session['username']
-	else:
-		flash('wrong password!')
+	for instance in s.query(User).order_by(User.username):#.filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
+		print instance.username
+	# result = query.first()
+	# if result:
+	# 	session['logged_in'] = True
+	# 	session['username'] = POST_USERNAME
+	# 	print session['username']
+	# else:
+	# 	flash('wrong password!')
 	return redirect(url_for('home'))
 
 
