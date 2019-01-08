09fcabe89cb35e525899e3d780b2eab20e2cb6cb
Ben Brady <bradybt@umich.edu>
2019-01-07 22:56:29 -0800

    Revert "I need to do a lot of refactoring :(((("
    This reverts commit c7f6e00e1056c5435d6b41ba3c38203d2dcc3f13.

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, flash, redirect, url_for
+from flask import Flask, render_template, request, session, flash
 import cPickle as pickle
 import substitute as sub
 import os
@@ -18,14 +18,12 @@ def home():
 
 @app.route('/<username>')
 def teamPage(username):
-
-	if not session.get('logged_in'):
-		return home()
+    if not session.get('logged_in'):
+        return home()
 	users = pickle.load(open("playerInfo.p", "rb"))
-
 	for user in users:
 		if username == user.name:
-			return render_template('ben.html', user=user, teamName = username, sessionUser=session.get('username'))
+			return render_template('ben.html', user=user, teamName = username)
 	return 'error'
 
 
@@ -39,9 +37,11 @@ def subPlayers(username):
 		attrMap = pickle.load(open("attrMap.p", "rb"))
 		playerName = request.form.get("subbed-player-name",None)
 		playerAttr = request.form.get("subbed-player-attr",None)
+		skipFlag = 0;
 
 		for user in users:
 			for player in user.team:
+
 				if player.name == playerName and player.attribute == attrMap[playerAttr]:
 					return render_template('ben.html', user=user, teamName = username)
 
@@ -52,7 +52,7 @@ def subPlayers(username):
 				return render_template('ben.html', user=user, teamName = username)
 	else:
 		user = sub.moveFromBench(playerName,username)
-		return render_template('ben.html', user=user, teamName=username, )
+		return render_template('ben.html', user=user, teamName=username)
 	return 'error'
 
 @app.route("/login", methods=['POST'])
@@ -66,18 +66,30 @@ def login():
 	result = query.first()
 	if result:
 		session['logged_in'] = True
-		session['username'] = POST_USERNAME
-		print session['username']
 	else:
 		flash('wrong password!')
-	return redirect(url_for('home'))
+	return home()
 
 
 @app.route("/logout")
 def logout():
     session['logged_in'] = False
-    session.pop('username', None)
     return home()
+
+@app.route('/test')
+def test():
+ 
+    POST_USERNAME = "python"
+    POST_PASSWORD = "python"
+ 
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
+    result = query.first()
+    if result:
+        return "Object found"
+    else:
+        return "Object not found " + POST_USERNAME + " " + POST_PASSWORD
 
 if __name__ == "__main__":
 	app.secret_key = os.urandom(12)
