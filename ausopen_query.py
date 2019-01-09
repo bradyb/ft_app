adafb68083c47a222bdc845c7b0bdea20a62efb5
Ben Brady <bradybt@umich.edu>
2019-01-08 22:30:08 -0800

    Added better methods for getting the match stats

---
+++ ausopen_query.py
@@ -0,0 +1,27 @@
+import constants
+
+import pprint as pp
+import requests
+
+def GetDayResults():
+	response = requests.get(constants.GET_RESULTS_URL, 
+							headers=constants.GET_RESULTS_HEADER)
+	return [ match['match_id'].encode("utf-8") 
+				for match in response.json()['matches']]
+
+def GetMatch():
+	response = requests.get(constants.GET_MATCH_URL, 
+							headers=constants.GET_MATCH_HEADER)
+	return response.json()
+
+def GetMatchStats(match_dict):
+	"""Input must be the result of GetMatch"""
+	return match_dict['stats']['key_stats'][0]['sets'][-1]
+
+def GetMatchPlayerNames(match_dict):
+	"""Input must be the result of GetMatch"""
+	return {'playerA': match_dict['teams'][0]['players'][0]['full_name'].encode("utf-8"),
+			'playerB': match_dict['teams'][1]['players'][0]['full_name'].encode("utf-8")}
+
+if __name__ == "__main__":
+	pp.pprint(GetMatchPlayerNames(GetMatch()))
---
+++ constants.py
@@ -0,0 +1,26 @@
+###########
+# Constants for gettings results from matches for a day.
+###########
+
+GET_RESULTS_HEADER = {
+	'Accept': 'application/json, text/plain, */*',
+	'Origin': 'https://ausopen.com',
+	'Referer': 'https://ausopen.com/results',
+	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36'
+}
+
+GET_RESULTS_URL = 'https://prod-scores-api.ausopen.com/year/2019/period/Q/day/2/results'
+
+###########
+# Constants for gettings results from matches for a day.
+###########
+
+GET_MATCH_HEADER = {
+	'accept': 'application/json, text/plain, */*',
+	'accept-encoding': 'gzip, deflate, br',
+	'accept-language': 'en-US,en;q=0.9',
+	'origin': 'https://ausopen.com',
+	'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36'
+}
+
+GET_MATCH_URL = 'https://prod-scores-api.ausopen.com/match-centre/PS120'
