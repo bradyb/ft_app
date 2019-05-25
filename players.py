67f31127d15fd69aa53f0bfb85856a823050dc05
bees <brady.ben12@gmail.com>
2019-05-25 14:31:03 -0700

    intitating the french open

--- players.py
+++ players.py
@@ -1,34 +1,23 @@
 import cPickle as pickle
 from playerTypes import fPlayer, tPlayer
 
-
-
 def initTourney():
 
 	attrMap = pickle.load(open( "attrMap.p", "rb" ))
-
 	tourneyFile = open('tInfo.txt', 'r')
-
 	numPlayers = int(tourneyFile.readline())
-
 	tourneyFile.readline()	
-
 	playerList = list()
-
 	# the tournament file needs to be formatted correctly in order for this to work 
 	for i in range(0, numPlayers):
-
 		playerList.append(fPlayer(tourneyFile.readline().rstrip()))
-
 		for j in range(0,7):
-
 			line = tourneyFile.readline().rstrip()
 			lineData = line.split(' ')
-			if int(lineData[4]) == 0: #if they are not a bench player
-				playerList[i].team.append(tPlayer(lineData[1] + " " + lineData[2], lineData[0], attrMap[lineData[3]]))
+			if int(lineData[-1]) == 0: #if they are not a bench player
+                            playerList[i].team.append(tPlayer(" ".join(lineData[1:-2]), lineData[0], attrMap[lineData[-2]]))
 			else:
-				playerList[i].bench.append(tPlayer(lineData[1] + " " + lineData[2], lineData[0], attrMap[lineData[3]]))
-
+                            playerList[i].bench.append(tPlayer(" ".join(lineData[1:-2]), lineData[0], attrMap[lineData[-2]]))
 		tourneyFile.readline()
 
 	pickle.dump( playerList, open( "playerInfo.p", "wb" ) )
