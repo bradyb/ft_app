5113466102954450702f73b283413c05975d3c0e
Ben Brady <brady.ben12@gmail.com>
2022-02-28 22:43:40 -0800

    Filled out some of the stat scoring logic. I think I need to start thinking about the DB and how I'm marking the player as active. Maybe I have a one off job that updates the bit.

--- rewrite/storage/stats.py
+++ rewrite/storage/stats.py
@@ -1,4 +1,5 @@
 # Stat objects for tennis players.
+from abc import ABC, abstractmethod
 from enum import Enum
 
 
@@ -20,3 +21,67 @@ class Stat(Enum):
     BREAK_POINTS_WON = 7
     BREAK_POINTS_SAVED = 8
     BREAK_POINTS_LOST = 9
+
+
+class MatchStat(ABC):
+
+    def __init__(self, position: Position):
+        self.position = position
+
+    @abstractmethod
+    def compute_score(self):
+        pass
+
+
+class ServeMatchStat(MatchStat):
+
+    def __init__(self, aces: int, double_faults: int):
+        super().__init__(Position.SERVE)
+        self.aces = aces
+        self.double_faults = double_faults
+
+    def compute_score(self):
+        return 3 * (self.aces - self.double_faults)
+
+
+class PowerMatchStat(MatchStat):
+
+    def __init__(self, winners: int, unforced_errors: int):
+        super().__init__(Position.POWER)
+        self.winners = winners
+        self.unforced_errors = unforced_errors
+
+    def compute_score(self):
+        return 2 * self.winners - self.unforced_errors
+
+
+class DefenseMatchStat(MatchStat):
+
+    def __init__(self, second_serves_won_pct: int):
+        super().__init__(Position.DEFENSE)
+        self.second_serves_won_pct = second_serves_won_pct
+
+    def compute_score(self):
+        return self.second_serves_won_pct
+
+
+class ReturnMatchStat(MatchStat):
+
+    def __init__(self, returning_points_won_pct: int):
+        super().__init__(Position.RETURN)
+        self.returning_points_won_pct = returning_points_won_pct
+
+    def compute_score(self):
+        return self.returning_points_won_pct
+
+
+class MindMatchStat(MatchStat):
+
+    def __init__(self, break_points_won: int, break_points_saved: int, break_points_lost: int):
+        super().__init__(Position.MIND)
+        self.break_points_won = break_points_won
+        self.break_points_saved = break_points_saved
+        self.break_points_lost = break_points_lost
+
+    def compute_score(self):
+        return 5 * (self.break_points_won + self.break_points_saved) - 3 * self.break_points_lost
--- rewrite/storage/teams.py
+++ rewrite/storage/teams.py
@@ -1,10 +1,10 @@
 # Defines the Team object to track the teams of the human players.
 from datetime import date
-from stats import Position, Stat
+from stats import Position, MatchStat
 from typing import Dict
 
 PlayerToPosition = Dict[str, Position]
-StatToValue = Dict[Stat, int]
+PositionToMatchStat = Dict[Position, MatchStat]
 
 
 class Team:
@@ -15,14 +15,35 @@ class Team:
         self.lineup = lineup
         self.bench = bench
         self.score = 0
+        self._init_player_to_date_to_match_stat()
 
-    def has_player(self, name: str) -> None:
+    def _init_player_to_date_to_match_stat(self):
+        self.player_to_date_to_match_stat = {}
+        for player_name, _ in self.lineup.items():
+            self.player_to_date_to_match_stat[player_name] = {}
+        for player_name, _ in self.bench.items():
+            self.player_to_date_to_match_stat[player_name] = {}
+
+    def _has_player(self, name: str) -> None:
         return name in self.lineup or name in self.bench
 
-    def update_player(self, match_date: date, name: str, match_stats: StatToValue):
-        if not self.has_player(name):
+    def _get_player_position(self, name: str):
+        if name in self.lineup:
+            return self.lineup[name]
+        else:
+            return self.bench[name]
+
+    def update_player(self, match_date: date, name: str, match_stats: PositionToMatchStat):
+        if not self._has_player(name):
             return
-        # TODO: Add logic for adding a match to this team.
+        position = self._get_player_position(name)
+        self.player_to_date_to_match_stat[name][match_date] = match_stats[position]
 
     def move_to_lineup(self, name: str):
         # TODO: Add logic for moving a player from the bench to the lineup.
+        pass
+
+    def get_current_score(self):
+        # TODO: How this is computed is dependent on how we represent/store
+        # when someone is benched.
+        pass
