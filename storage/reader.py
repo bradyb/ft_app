cb0f211d63b7537779e3a1373e54c54e050ace11
bradyb <brady.ben12@email.com>
2022-03-07 23:23:46 -0800

    Adds some more details to reader.py.

--- storage/reader.py
+++ storage/reader.py
@@ -1,3 +1,13 @@
+from storage.tournaments import Tournament
+
+
 def read_tournament():
-    # Implement reading a tournament from the SQLite3 db.
+    # TODO: Implement reading a tournament from the SQLite3 db.
+    tournament = None
+    return dict(tournament)
+    pass
+
+
+def initiate_tournament():
+    # TODO: Read teams from structured text and write to db.
     pass
